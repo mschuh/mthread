90f0fd0d55e8fc88c25d4a2bf50ef9cdaf38a512
Nogueira5 <fadsnogueira@gmail.com>
2014-04-23 06:04:57 -0300

    Update linkedlist.h

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -6,12 +6,12 @@
 #include "mdata.h"
 
 TCBList* CreateList(void);
-TCB* Pop(TCBList* list);
+TCB* Pop(TCBList** list);
 TCBList* Insert(TCBList* list, TCB* newElement);
 TCBList* InsertSorted(TCBList* list, TCB* newElement);
 TCBList* SortList (TCBList* list);
-void Remove(TCBList* list, int tid);
-TCB* RemoveWaiting(TCBList* list, int waitingThread);
+void Remove(TCBList** list, int tid);
+TCB* RemoveWaiting(TCBList** list, int waitingThread);
 void DeleteList(TCBList* list);
 int searchTID (TCBList* list, int tid);
 int searchWaiting(TCBList* list, int tid);
